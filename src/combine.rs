c3fd0769e9d776a32b7a314cd1269501fdabd7f6
Christian Sakai <christianmsakai@gmail.com>
2019-02-11 00:19:43 -0500

    WIP Cleanup

--- src/combine.rs
+++ src/combine.rs
@@ -1,37 +1,108 @@
-use std::io;
-use std::io::Read;
+use image;
+use image::GenericImageView;
+
+use std::path;
 use std::env;
-use std::fs;
+use crate::config::Config;
 
-mod config;
-mod combine;
+type Image = image::ImageBuffer<image::Rgba<u8>, Vec<u8>>;
 
-const HELP: &'static str = r#"
-No config file path provided.
-$ cargo run -- path_to_config_file
-"#;
+pub fn combine(config: Config) {
+    let cell_width = config.width / config.cols;
+    let cell_height = config.height / config.rows;
 
-fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() <= 1 {
-        return Err(io::Error::new(
-            io::ErrorKind::NotFound,
-            HELP,
-        ));
+    let mut images_matrix: Vec<Vec<Option<Image>>> = Vec::new();
+    for _ in 0..config.rows + 1 {
+        let mut images_row = Vec::new();
+        for _ in 0..config.cols + 1 {
+            images_row.push(None);
+        }
+
+        images_matrix.push(images_row);
     }
 
-    let config_file = &args[1];
+    for img in &config.images {
+        // TODO: check error here
+        let mut absolute_path = env::current_dir().unwrap();
+        absolute_path.push(&img.path);
 
-	let mut file = fs::File::open(config_file)?;
-    let mut string = String::new();
-    file.read_to_string(&mut string)?;
+        let image_file = image::open(&absolute_path)
+            .unwrap();
 
-    let config = config::Config::new(&string)
-        .expect("Parsing config file failed");
+        println!("img.path {:?}", absolute_path);
+        // TODO: usize vs u32
+        let resized_img = resize(
+            cell_width as u32,
+            cell_height as u32,
+            &image_file,
+            // TODO: replace with background_color
+            image::Rgba([100, 100, 100, 0]),
+        );
 
-    combine::combine(config);
+        images_matrix[img.row][img.col] = Some(resized_img);
+    }
 
-    Ok(())
+    let mut combined: Image = image::ImageBuffer::new(config.width as u32, config.height as u32);
+    for (x, y, pixel) in combined.enumerate_pixels_mut() {
+        let img_row = (y / cell_height as u32); 
+        let img_col = (x / cell_width as u32);
+
+        let img = &images_matrix[img_row as usize][img_col as usize];
+        match img {
+            Some(image) => {
+                let img_pixel = image.get_pixel(x - (img_col * cell_width as u32), y - (img_row * cell_height as u32));
+
+                *pixel = *img_pixel;
+            }
+
+            _ => {
+                // TODO: replace with background_color
+                *pixel = image::Rgba([100, 100, 100, 0]);
+            }
+        }
+    }
+
+    combined.save(config.output).unwrap();
+}
+
+fn resize(
+    width: u32,
+    height: u32,
+    image: &image::DynamicImage,
+    pad: image::Rgba<u8>,
+) -> Image {
+    let resized_img = image.resize(width, height, image::FilterType::Nearest);
+    let mut padded_img = image::ImageBuffer::new(width, height);
+
+    let resized_img_dimensions = resized_img.dimensions();
+
+    // Pad image vertically
+    if resized_img_dimensions.0 == width {
+        let y_offset = (height - resized_img_dimensions.1) / 2;
+
+        for (x, y, pixel) in padded_img.enumerate_pixels_mut() {
+            if y < y_offset || y >= (y_offset + resized_img_dimensions.1) {
+                *pixel = pad;
+            } else {
+                *pixel = resized_img.get_pixel(x, y - y_offset);
+            }
+        }
+    }
+
+    // Pad image horizontally
+    else if resized_img_dimensions.1 == height {
+        let x_offset = (width - resized_img_dimensions.0) / 2;
+
+        for (x, y, pixel) in padded_img.enumerate_pixels_mut() {
+            if x < x_offset || x >= (x_offset + resized_img_dimensions.0) {
+                *pixel = pad;
+            } else {
+                *pixel = resized_img.get_pixel(x - x_offset, y);
+            }
+        }
+    }
+
+    padded_img
 }
 
 // fn func() {
@@ -46,9 +117,6 @@ fn main() -> io::Result<()> {
 //     let height = 600;
 //     let rows = 2;
 //     let cols = 3;
-
-//     let cell_width = width / cols;
-//     let cell_height = height / rows;
 
 //     let cat = image::open("./sample_images/cat.jpg").unwrap();
 //     let cat2 = image::open("./sample_images/cat2.jpg").unwrap();
---
+++ src/config.rs
@@ -0,0 +1,164 @@
+use std::collections::HashMap;
+use std::fmt;
+use std::error;
+
+#[derive(Debug)]
+pub struct ImageConfig {
+    pub row: usize,
+    pub col: usize,
+    pub path: String,
+}
+
+impl ImageConfig {
+    fn new(image_str: &str) -> Result<ImageConfig, ConfigError> {
+        let image_str_split: Vec<&str> = image_str
+            .split(',')
+            .collect();
+
+        let row = image_str_split.get(0);
+        let col = image_str_split.get(1);
+        let path = image_str_split.get(2);
+
+        match (row, col, path) {
+            (Some(row), Some(col), Some(path)) => {
+                let row = row.parse();
+                let col = col.parse();
+
+                match (row, col) {
+                    (Ok(row), Ok(col)) => {
+                        Ok(ImageConfig {
+                            row,
+                            col,
+                            path: (*path).to_owned(),
+                        })
+                    },
+                    _ => Err(ConfigError::ParseImageError),
+                }
+            },
+            _ => Err(ConfigError::ParseImageError),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Config {
+    pub output: String,
+    pub rows: usize,
+    pub cols: usize,
+    pub height: usize,
+    pub width: usize,
+    pub background_color: String,
+    pub images: Vec<ImageConfig>,
+}
+
+impl Config {
+    pub fn new(config_str: &str) -> Result<Config, ConfigError> {
+        let str_split = config_str.trim().split('\n');
+        let mut map = HashMap::new();
+        let mut images = Vec::new();
+
+        for line in str_split {
+            let key_val = line
+                .split('=')
+                .map(|el| el.trim())
+                .collect::<Vec<&str>>();
+
+            if key_val[0] == "IMAGE" {
+                let image = ImageConfig::new(key_val[1])?;
+                images.push(image);
+            } else {
+                let key = key_val[0].to_lowercase();
+                let val = key_val[1].to_owned();
+
+                map.insert(key, val);
+            }
+        }
+
+        let output = map.get("output")
+            .ok_or(ConfigError::NeedOutputError)?;
+
+        let rows = map.get("rows")
+            .ok_or(ConfigError::NeedRowsError)?
+            .parse::<usize>()
+            .map_err(|_| ConfigError::ParseRowsError)?;
+
+        let cols = map.get("cols")
+            .ok_or(ConfigError::NeedColsError)?
+            .parse::<usize>()
+            .map_err(|_| ConfigError::ParseColsError)?;
+
+        if rows < 1 || cols < 1 {
+            return Err(ConfigError::InvalidRowsColsError);
+        }
+
+        let height = map.get("height")
+            .ok_or(ConfigError::NeedHeightError)?
+            .parse::<usize>()
+            .map_err(|_| ConfigError::ParseHeightError)?;
+
+        let width = map.get("width")
+            .ok_or(ConfigError::NeedWidthError)?
+            .parse::<usize>()
+            .map_err(|_| ConfigError::ParseWidthError)?;
+
+        let background_color = map.get("background_color")
+            .ok_or(ConfigError::NeedBackgroundColorError)?;
+
+        if images.len() < 1 {
+            return Err(ConfigError::NeedImagesError);
+        }
+
+        Ok(Config {
+            output: output.to_string(),
+            rows,
+            cols,
+            height,
+            width,
+            background_color: background_color.to_string(),
+            images,
+        })
+    }
+}
+
+#[derive(Debug)]
+pub enum ConfigError {
+    NeedOutputError,
+    NeedRowsError,
+    ParseRowsError,
+    NeedColsError,
+    ParseColsError,
+    NeedHeightError,
+    ParseHeightError,
+    NeedWidthError,
+    ParseWidthError,
+    NeedBackgroundColorError,
+    NeedImagesError,
+    ParseImageError,
+    InvalidRowsColsError,
+}
+
+impl fmt::Display for ConfigError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ConfigError::NeedOutputError => write!(f, "Need OUTPUT in config"),
+            ConfigError::NeedRowsError => write!(f, "Need ROWS in config"),
+            ConfigError::ParseRowsError => write!(f, "Failed to parse ROWS"),
+            ConfigError::NeedColsError => write!(f, "Need COL in config"),
+            ConfigError::ParseColsError => write!(f, "Failed to parse COLS"),
+            ConfigError::NeedHeightError => write!(f, "Need HEIGHT in config"),
+            ConfigError::ParseHeightError => write!(f, "Failed to parse HEIGHT"),
+            ConfigError::NeedWidthError => write!(f, "Need WIDTH in config"),
+            ConfigError::ParseWidthError => write!(f, "Failed to parse WIDTH"),
+            ConfigError::NeedBackgroundColorError => write!(f, "Need BACKGROUND_COLOR in config"),
+            ConfigError::NeedImagesError => write!(f, "Need IMAGE in config"),
+            ConfigError::ParseImageError => write!(f, "IMAGE needs to be in format <row,col,path_to_image>"),
+            ConfigError::InvalidRowsColsError => write!(f, "ROWS or COLS cannot be lesser than 1"),
+        }
+    }
+}
+
+impl error::Error for ConfigError {
+    fn cause(&self) -> Option<&error::Error> {
+        None
+    }
+}
